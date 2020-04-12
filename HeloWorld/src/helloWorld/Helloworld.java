00dc535dcfb348894e37e0636e911ce304dcd383
Amol Pardhi <Amol Pardhi@Amol>
2020-04-12 13:40:01 +0530

    Change done to code

--- HeloWorld/src/helloWorld/Helloworld.java
+++ HeloWorld/src/helloWorld/Helloworld.java
@@ -3,7 +3,19 @@ package helloWorld;
 public class Helloworld {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hello World");
+		
+		System.out.println("Hello "+"World");
+		
+		System.out.println("Hello"+" World");
+		
+		String a = "Hello World";
+		System.out.println(a);
+		
+		String b = "Hello";
+		System.out.println(b + " World");
+		
 	}
 
 }
